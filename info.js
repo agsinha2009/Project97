f1d2d4e9ecd03211a7f219592e32cf2d2188b00a
agsinha2009 <101357307+agsinha2009@users.noreply.github.com>
2022-08-29 18:59:27 +0200

    Add files via upload

---
+++ info.js
@@ -0,0 +1,10 @@
+user_name =localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML="Welcome"+user_name;
+
+
+function logout(){
+    window.location="index.html";
+    localStorage.removeItem("LogInName");
+}
+
+
---
+++ main.js
@@ -0,0 +1,5 @@
+function LogIn(){
+    user_name=document.getElementById("user_name").value;
+    localStorage.setItem("user_name",user_name);
+    window.location="info.html";
+}
